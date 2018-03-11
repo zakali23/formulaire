483ea1b304bd9e5e48b274477ce325369f5ec4e2
zakali23 <36405953+zakali23@users.noreply.github.com>
2018-03-11 13:26:52 +0100

    Update form.php
    Ajouter la validation serveur

--- form.php
+++ form.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $lastName = $firstName = $tel = $email = $textarea ="";
 
 function securisation($infos){
@@ -15,5 +17,37 @@ $tel = securisation($_POST['tel']);
 $email = securisation($_POST['email']);
 $textarea = securisation($_POST['textarea']);
 
-echo 'Bonjour Monsieur :'.$LastNAme.'</br>';
+$errors= [];
+if (!array_key_exists('lastName', $_POST) || $lastName == '') {
+  $errors['lastName']= "Please enter your Last NAme";
+}
+if (!array_key_exists('firstName', $_POST) ||  $firstName == '') {
+  $errors['firstName']="Please enter your first Name";
+}
+if (!array_key_exists('tel', $_POST) ||  $tel == '') {
+  $errors['tel']="Please enter your Number phone";
+}
+if (!array_key_exists('email', $_POST) ||  $email == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+  $errors['email']="Please enter your Valide Email";
+}
+if (!array_key_exists('textarea', $_POST) ||  $textarea == '') {
+  $errors['textarea']="Please enter your Message";
+}
+
+session_start();
+
+if (!empty($errors)) {
+  $_SESSION['errors'] = $errors;
+  $_SESSION['champs'] = $_POST;
+  header('location: tester.php');
+}
+else {
+  $_SESSION['success'] = 1;
+  $header="l'expediteur : $email";
+  mail('zakaria.zekraoui@gmail.com', 'Formulaire de contact', $textarea, $header);
+header('location: tester.php');
+}
+
+
+
 ?>
